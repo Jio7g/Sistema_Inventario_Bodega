1f6dec9ddf59a3d5303875b727815e185a241ade
Jio7g <cramosg8@miumg.edu.gt>
2023-11-02 14:22:52 -0600

    Se mejoro el frontent, se agrego busqueda y paginación en lista de inventario

--- inventory/models.py
+++ inventory/models.py
@@ -2,10 +2,12 @@ from django.db import models
 
 class Producto(models.Model):
     codigo = models.CharField(max_length=50, unique=True)
+    codigo_de_barras = models.CharField(max_length=20, unique=True, default='0')
     nombre = models.CharField(max_length=100)
     cantidad_existente = models.IntegerField()
     precio_compra = models.DecimalField(max_digits=10, decimal_places=2)
     precio_venta = models.DecimalField(max_digits=10, decimal_places=2)
+
 
 class RegistroSalida(models.Model):
     producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
--- inventory/views.py
+++ inventory/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Producto, RegistroSalida
 from .forms import ProductoForm, RegistroSalidaForm
+from django.core.paginator import Paginator
+from django.db.models import Q
 
 def ingresar_producto(request):
     if request.method == 'POST':
@@ -18,16 +20,41 @@ def registrar_salida(request):
         if form.is_valid():
             codigo = form.cleaned_data['codigo']
             cantidad = form.cleaned_data['cantidad']
-            producto = Producto.objects.get(codigo=codigo)
+
+            try:
+                # Intenta encontrar un producto con el código de barras escaneado.
+                producto = Producto.objects.get(codigo_de_barras=codigo)
+            except Producto.DoesNotExist:
+                # Si el producto no se encuentra, muestra un mensaje de error.
+                error_message = 'Producto no encontrado. Escanea otro código de barras.'
+                return render(request, 'registrar_salida.html', {'form': form, 'error_message': error_message})
+
             if producto.cantidad_existente >= cantidad:
+                # Si hay suficiente cantidad en el inventario, procesa la salida.
                 producto.cantidad_existente -= cantidad
                 producto.save()
                 RegistroSalida.objects.create(producto=producto, cantidad=cantidad)
                 return redirect('consultar_inventario')
+            else:
+                # Si la cantidad escaneada es mayor que la cantidad en inventario, muestra un mensaje de error.
+                error_message = 'Cantidad insuficiente en el inventario.'
+                return render(request, 'registrar_salida.html', {'form': form, 'error_message': error_message})
+
     else:
         form = RegistroSalidaForm()
+
     return render(request, 'registrar_salida.html', {'form': form})
 
 def consultar_inventario(request):
     productos = Producto.objects.all()
-    return render(request, 'consultar_inventario.html', {'productos': productos})
+    query = request.GET.get('q')  # Obtén el término de búsqueda del parámetro GET 'q'
+
+    if query:
+        # Utiliza Q para realizar una consulta OR entre nombre y código
+        productos = productos.filter(Q(nombre__icontains=query) | Q(codigo__icontains=query))
+
+    paginator = Paginator(productos, 5)  # Divide la lista de productos en páginas con 5 productos por página
+    page = request.GET.get('page')
+    productos = paginator.get_page(page)
+
+    return render(request, 'consultar_inventario.html', {'productos': productos, 'query': query})
